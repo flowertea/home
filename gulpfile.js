3c50eb5bd9384f6fda287c97f2aa1963ee482a1f
yitian <yi.t.xu@mail.mcgill.ca>
2015-07-15 20:08:15 -0400

    First commit :3

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+
+jade = require('gulp-jade'),
+sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+	gulp.src('./src/sass/*.sass')
+	.pipe(sass())
+	.pipe(gulp.dest('./assets/css/'));
+});
+
+gulp.task('jade', function () {
+	gulp.src('./src/jade/*.jade')
+	.pipe(jade())
+	.pipe(gulp.dest('./'));
+});
