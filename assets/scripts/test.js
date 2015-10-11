cbc4d970eedf6eff884e68b713714ba67addf4e6
yitian <yi.t.xu@mail.mcgill.ca>
2015-10-11 14:03:48 -0400

    update

--- assets/scripts/test.js
+++ assets/scripts/test.js
@@ -10,12 +10,32 @@ var testimonials = {
 		message: 'Mariana did a soul drawing for me. What a gorgeous and accurate depiction of the truth of who I am. Water and earth energies are strong for me, so I love all the green and blue she used. The orange also represents my love of beauty and feeling good. As I gaze at the drawing, I feel my energetic field align itself from the centre, from my heart. It feels comforting and powerful in my body. This is a perfect time for me to have something tangible to remind me of who I am, as I step into adulthood and decide how I would like to participate in this world. It reminds me of one of my favourite childhood movies, Thumbelina, the fairy born of a flower who does her best to fit into this world. Her message that accompanied the drawing was simple yet VERY potent for me, at this time. I look forward to watching her intuitive gifts grace this world.',
 		date: '10/8/2015',
 	},{
+		user: 'Nicole Estrella',
+		message: 'This beautiful lady gave me an amazing reading. You are truly gifted!! It gave me so much insight to myself I was amazed. Thank you for all that you do. I highly suggest messaging her about a reading... you won\'t regret it',
+		date: '10/4/2015',
+	},{
 		user: 'Grandiose Messiahcomplex',
 		message: 'I received a free reading from Mariana yesterday, I am blown away by her accuracy and the information that she knew that I gave her zero knowledge of. She is the real deal and I intend to and am happy to pay for future advice. Thank you again Mariana, I can\'t thank you enough.',
 		date: '10/4/2015',
 	},{
+		user: 'Tiffany Tisdale',
+		message: 'I\'ve never had such a great reading from such a beautiful person in my entire life. This beautiful soul is unbelievable and i can\'t even put into words how great she is.....................thank you ...thank you so much.',
+		date: '10/3/2015',
+	},{
+		user: 'Amber Sage',
+		message: 'Mariana gave me the most amazing reading! She is truly gifted. Thank you so much!',
+		date: '10/3/2015',
+	},{
+		user: 'Janet Hope Horwitz',
+		message: 'I just had a reading with Mariana. Just from asking her one question, I got so much information not only about my soul\'s purpose, but what i need right now in my life and about my personality. She rally captured the essence of who I am. I just feel very understood and validated. It was just what I needed to know right now. She was very accurate about everything she said. I highly recommend her if you are feeling uncertainty in your life and would like some clarity.',
+		date: '10/3/2015',
+	},{
 		user: 'Rouyun Tan',
 		message: 'Mariana Seoane is a being of love and compassion<br>During the free reading in which Mariana kindly blessed to many and myself, she tuned into my akashic records and gave me valuable information.<br>Mariana conveyed messages from Archangel Michael and Azrael to me too. She is familiar with processes and warmly encouraged me to do what I have to do.<br>Truly grateful to connect with her',
+		date: '10/2/2015',
+	},{
+		user: 'Natalie Bushnell',
+		message: 'Mariana gave me an intuitive reading this morning. . . I was open to whatever came through and boy did she unwrap my world. She helped me clarify some questions I have had for some time but really didn’t want to face. Funny thing was it had nothing to do with what I asked but she picked up on it anyway. . . And gave me the tools to heal it. She is a shining star and very gifted... she certainly brings final closure to whatever it is you have been trying to determine at the crossroads of life.... thank you again sweet girl... I will update you once it all comes full circle. Hugs',
 		date: '10/2/2015',
 	},{
 		user: 'Brigitta Rabold',
@@ -24,6 +44,26 @@ var testimonials = {
 	},{
 		user: 'Dyana Tran',
 		message: 'Thank you Mariana. You are truly a gifted person. You had me in tears!! I appreciate your time and energy and especially your advice and insights. Everything was accurate and spot on! I know you will help so many people with your gifts. Thank you again. Love and light!',
+		date: '10/2/2015',
+	},{
+		user: 'Helena Horvat',
+		message: 'I had a lovely chit chat with Mariana. She is very sweet, spot on and funny!! If you want a mini reading, she\'s your girl! I\'ve received messages from my Andromedan family, guides and Angels, and apparently my Andromedan self is having a party up there coz when I asked for a name it sounded like a mushroom. Oh to be in Higher Dimensions...So much fun! Anyway, check her out, she\'s amazing!! Support her work, she\'s a Superhero!',
+		date: '10/2/2015',
+	},{
+		user: 'Abby Chavez',
+		message: 'I just had a reading with Mariana and OMG! I am in awe right now! I learned that I am a Sirian being and what my soul\'s purpose is. I also learned that I need to trust the messages I get and that I am communicating with my guides (even though it feels like I\'m talking to myself lol)! I learned quite a bit about what I needed to know at the moment. She is great and very nice! I felt like I was talking to a close girlfriend. ',
+		date: '10/2/2015',
+	},{
+		user: 'Shannon Christine',
+		message: 'I had a reading from Mariana today! She was amazing and so helpful. I wasn\'t in a great mood, life had me very down and she turned my frown upside down. I was amazed by the answers she gave...she was giving them right before I asked too!!',
+		date: '10/2/2015',
+	},{
+		user: 'Amanda Bolinder-Thoma',
+		message: 'I just had a quick reading from Mariana as well. She is able to tap in very quickly. In our short session she gave me a lot to work with. She was able to answer specific questions. I was also triggered 3 times so now I have 3 things to tap on and I am excited to do the work. She has a very bright future in readings.',
+		date: '10/2/2015',
+	},{
+		user: 'Jen White',
+		message: 'Mariana thank you so much for the amazing reading last night! You are truly gifted in so many ways. I learned so much about my past lives and the reasons for my health issues. I will definitely be contacting you for a soul drawing soon!',
 		date: '10/2/2015',
 	},{
 		user: 'Natacha Simpson',
