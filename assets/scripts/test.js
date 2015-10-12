07169369539345e58580cde533bc80588ee461ca
yitian <yi.t.xu@mail.mcgill.ca>
2015-10-12 16:39:47 -0400

    reorder

--- assets/scripts/test.js
+++ assets/scripts/test.js
@@ -2,12 +2,12 @@ var testimonials = {
 	index: -1,
 	length: 0,
 	data: [{
-		user: 'Lygia Romou-Osorio Lobato',
-		message: 'Thank you Mariana Claire, for your reading. You were very fast and on point.',
-		date: '10/11/2015',
-	},{
 		user: 'L. Jewell Johnson',
 		message: 'I love your validation that \'all I need to do is remember\' Mariana Claire! and all the wonderful source aspect, life purpose, negative imprint clarifications for me to explore and connect with an integrate into my authentic being.. infinite divine love, light and blessings to you precious on.',
+		date: '10/11/2015',
+	},{
+		user: 'Lygia Romou-Osorio Lobato',
+		message: 'Thank you Mariana Claire, for your reading. You were very fast and on point.',
 		date: '10/11/2015',
 	},{
 		user: 'Erin Aslan',
